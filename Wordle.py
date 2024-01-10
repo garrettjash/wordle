528a6e45dd7210b5e31cab64b5ad4306bd3c15ec
garrettjash <garrettashcroft@gmail.com>
2024-01-10 14:39:38 -0700

    Displays Word

--- Wordle.py
+++ Wordle.py
@@ -13,14 +13,20 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    gw = WordleGWindow()
 
     def enter_action(s):
         gw.show_message("You have to implement this method.")
 
-    gw = WordleGWindow()
+    # This sets the first row to the random word
+    for iCount in range(N_COLS): #Iterates through the number of columns (5)
+        gw.set_square_letter(0, iCount, randomWord[iCount])
+
     gw.add_enter_listener(enter_action)
 
 # Startup code
 
 if __name__ == "__main__":
+    # Hardcoded random word for display test
+    randomWord = 'zesty'
     wordle()
